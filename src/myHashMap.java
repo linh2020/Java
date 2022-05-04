2d845832473c16ec9dafa9b31b069ca27f669c91
Linh Huynh <70009294+linh2020@users.noreply.github.com>
2022-05-03 17:17:11 -0700

    test 1

---
+++ src/myHashMap.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class myHashMap {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm = new HashMap<String,Integer>();
+
+        //testingggg
+    }
+}
